3cb55ecb0c6dd60a20ade35b64adf2fd3000141b
Grace Letiwa <133238809+GLetiwa@users.noreply.github.com>
2023-11-01 20:00:08 +0300

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-regex = /\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/
-
-# Open and read the log file
-File.open(ARGV[0]).each do |line|
-  match_data = line.match(regex)
-  if match_data
-    sender = match_data[1]
-    receiver = match_data[2]
-    flags = match_data[3]
-    puts "#{sender},#{receiver},#{flags}"
-  end
-end
+puts ARGV[0].scan(/\[(?:from|to|flags):(\+?[|a-zA-Z\d:-]+?)\]/).map(&:first).join(',')
