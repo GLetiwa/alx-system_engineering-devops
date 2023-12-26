45f395508d61f4a8bea1808d0d1b6465971765d0
GLetiwa <graceletiwa@gmail.com>
2023-12-26 02:48:54 -0800

    Config Mngt

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Using Puppet to install flask from pip3
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
 }
