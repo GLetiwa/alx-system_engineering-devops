0064e024685abdbdce936e65bbe292a0ff072aef
GLetiwa <graceletiwa@gmail.com>
2023-12-26 02:54:32 -0800

    Config Mngt

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # 2-execute_a_command.pp
 
-exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => '/bin:/usr/bin',
-  refreshonly => true,
+exec { 'killmenow_process':
+  command => '/usr/bin/pkill -f killmenow',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => '/usr/bin/pgrep -f killmenow',
 }
